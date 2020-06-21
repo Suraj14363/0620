ce001be6e481e50a2066038259809c34cc3e4120
91957 <91957@DESKTOP-F76IHI6>
2020-06-21 08:23:46 +0530

    C3

--- GitProj/src/packone/Sample.java
+++ GitProj/src/packone/Sample.java
@@ -4,7 +4,6 @@ public class Sample {
 
 	public static void main(String[] args) {
 		System.out.println("this is first git program");
-		System.out.println("changes were made");
 		System.out.println("Kishore");
 	}
 
